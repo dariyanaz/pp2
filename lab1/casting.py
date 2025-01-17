7efb52644644368d19a608d5c87360769432e562
dariyanaz <dariyanazarova0@gmail.com>
2025-01-17 20:44:38 +0500

    casting

---
+++ lab1/casting.py
@@ -0,0 +1,22 @@
+#Python Casting
+
+#Casting in python is therefore done using constructor functions:
+# int() - constructs an integer number from an integer literal, a float literal (by removing all decimals), or a string literal (providing the string represents a whole number)
+# float() - constructs a float number from an integer literal, a float literal or a string literal (providing the string represents a float or an integer)
+# str() - constructs a string from a wide variety of data types, including strings, integer literals and float literals
+
+#Integers:
+x = int(3)   # x will be 3
+y = int(4.5) # y will be 4
+z = int("5") # z will be 5
+
+#Floats:
+x = float(1)     # x will be 1.0
+y = float(1.8)   # y will be 1.8
+z = float("5")   # z will be 5.0
+w = float("4.2") # w will be 4.2
+
+#Strings:
+x = str("s1") # x will be 's1'
+y = str(2)    # y will be '2'
+z = str(3.0)  # z will be '3.0'

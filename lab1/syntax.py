936dcbd6426fbeaf40f89ba323350d8792c418d9
dariyanaz <dariyanazarova0@gmail.com>
2025-01-17 19:29:48 +0500

    syntax

---
+++ lab1/syntax.py
@@ -0,0 +1,19 @@
+#1
+if 5 > 2:
+  print("Five is greater than two!")
+
+#2. Syntax Error
+#if 5 > 2:
+#print("Five is greater than two!")
+
+#3
+if 5 > 2:
+ print("Five is greater than two!") 
+if 5 > 2:
+        print("Five is greater than two!")
+        
+#4. Syntax Error
+#if 5 > 2:
+ #print("Five is greater than two!")
+       # print("Five is greater than two!")
+       
